caa2e7ee000b745f9fc6c7908857a07012e4d8aa
krosk <ahe.krosk@gmail.com>
2016-03-18 18:27:51 +0100

    Add: turn to target and move

--- src/Engine.cpp
+++ src/Engine.cpp
@@ -7,8 +7,8 @@
 void Engine::addDummyEntity(int uid)
 {
 	PositionComponent &p = positionComponentList.add(uid, PositionComponent(uid, 0, 0, 0, 0));
-	VelocityComponent &v = velocityComponentList.add(uid, VelocityComponent(uid, 1, 30));
-	PositionTargetComponent &t = positionTargetComponentList.add(uid, PositionTargetComponent(uid, 5, 0, 0, 0));
+	VelocityComponent &v = velocityComponentList.add(uid, VelocityComponent(uid, 5, 30));
+	PositionTargetComponent &t = positionTargetComponentList.add(uid, PositionTargetComponent(uid, -30, -30, 0, 0));
 	MovementNode n(uid, p, v, t);
 	moveSystem.addNode(n);
 }
