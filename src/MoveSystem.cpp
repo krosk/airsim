4bb9654a2f260fff912cac744a62195d7da5d12e
krosk <ahe.krosk@gmail.com>
2016-03-18 18:27:59 +0100

    Add: turn to target and move

--- src/MoveSystem.cpp
+++ src/MoveSystem.cpp
@@ -1,6 +1,7 @@
 #include "MoveSystem.h"
 
 #include <iostream>
+#include <cmath>
 
 void MoveSystem::update(int currentStep)
 {
@@ -13,6 +14,38 @@ void MoveSystem::update(int currentStep)
 		const VelocityComponent &v = n.velocity;
 		const PositionTargetComponent &t = n.target;
 		
-		p.positionX += v.velocityFB;
+		// estimate XY angle
+		int deltaX = t.targetX - p.positionX;
+		int deltaY = t.targetY - p.positionY;
+		double pathRad = std::atan2(deltaY, deltaX);
+		int pathR = (int) (pathRad * 180. / M_PI);
+		int deltaR = pathR - p.positionR;
+		if (deltaR <= -180)
+		{
+		    deltaR = -360 - deltaR;
+		}
+		else if (deltaR >= 180)
+		{
+		    deltaR = 360 - deltaR;
+		}
+		
+		// move
+		if (std::abs(deltaR) > v.velocityR)
+		{
+		    p.positionR = deltaR > 0 ? p.positionR + v.velocityR :
+		                  deltaR < 0 ? p.positionR - v.velocityR :
+		                  pathR;
+		}
+		else
+		{
+		    p.positionR = pathR;
+		    p.positionX += (int) (v.velocityFB * std::cos(pathRad));
+		    p.positionY += (int) (v.velocityFB * std::sin(pathRad));
+		}
+		
+		std::cout << p.positionX << " " << p.positionY 
+		    << " " << p.positionR << "\n";
+		std::cout << t.targetX << " " << t.targetY 
+		    << " " << pathR << "\n";
 	}
 }
