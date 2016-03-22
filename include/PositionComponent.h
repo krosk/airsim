00a5c76bc58f11764999c71131ab87fde35f6f40
krosk <ahe.krosk@gmail.com>
2016-03-22 18:42:48 +0100

    Add: movement flag

--- include/PositionComponent.h
+++ include/PositionComponent.h
@@ -7,7 +7,7 @@ class PositionComponent
 	int positionX;
 	int positionY;
 	int positionZ;
-	int rotation;
+	int positionR;
 	
-	PositionComponent(int id, int x, int y, int z, int r) : uid(id), positionX(x), positionY(y), positionZ(z), rotation(r) {};
+	PositionComponent(int id, int x, int y, int z, int r) : uid(id), positionX(x), positionY(y), positionZ(z), positionR(r) {};
 };
