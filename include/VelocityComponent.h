66a31a94825abbd400de3de9e4a62d27e3d182db
krosk <ahe.krosk@gmail.com>
2016-03-26 18:10:41 +0100

    Add: aircraft system

--- include/VelocityComponent.h
+++ include/VelocityComponent.h
@@ -6,7 +6,6 @@ class VelocityComponent
 	const int uid;
 	int velocityFB;
 	int velocityR;
-	bool moving;
 	
-	VelocityComponent(int id, int fb, int r) : uid(id), velocityFB(fb), velocityR(r), moving(false) {};
+	VelocityComponent(int id, int fb, int r) : uid(id), velocityFB(fb), velocityR(r) {};
 };
